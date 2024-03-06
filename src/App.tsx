b3e62323378505ee87921de42a961aa8eba4a078
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-06 11:29:00 +0100

    Add footer component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header>
         <h1>Journal</h1>
       </Header>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Journal App - {currentYear}</p>
+    </footer>
+  );
+}
